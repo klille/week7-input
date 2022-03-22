f467e5145182e05f1419bda298d005ab5bfbd44d
ees-ja perekonnanimi <klille@tktk.eel~>
2022-03-22 21:47:44 +0200

    add movie

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.container');
+const movieTitle = document.querySelector('.userInputTitle');
+const moviePosterUrl = document.querySelector('.userInputPosterUrl');
+const btn = document.querySelector('.button');
+const movieTitleToDisplay = document.querySelector('.FavoriteMovieTitle');
+
+let titleInStorage = localStorage.getItem('title');
+let imageUrlInStorage = localStorage.getItem('imageUrl');
+
+if(titleInStorage && imageUrlInStorage) {
+    movieTitleToDisplay.textContent = titleInStorage;
+    container.style.backgroundImage = `linear-gradient(rgba(37, 37, 61, 0.5), rgba(77, 61, 61, 0.616)), 
+    url('${imageUrlInStorage}')`;
+}
+
+btn.addEventListener('click', () => {
+    let movieTitleInput = movieTitle.value;
+    let posterUrlInput = moviePosterUrl.value;
+    localStorage.setItem('title', movieTitleInput);
+    localStorage.setItem('imageUrl', posterUrlInput);
+    movieTitleToDisplay.textContent = movieTitleInput;
+    container.style.backgroundImage = `linear-gradient(rgba(37, 37, 61, 0.5), rgba(77, 61, 61, 0.616)),
+    url('${posterUrlInput}')`;
+    movieTitle.value = '';
+    moviePosterUrl.value = '';
+})
